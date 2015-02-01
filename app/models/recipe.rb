2d937a5506da3e8d0ae348e3cc57943c85a8b601
Roberta Reusch-Lewis <roberta@rigor.com>
2015-02-01 17:27:21 -0500

    add validation so a recipe must have at least one step

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,14 @@ class Recipe < ActiveRecord::Base
 	has_many :steps
 
 	validates_presence_of :name, :serving_size
+  validate :at_least_one_step?
+
+  accepts_nested_attributes_for :steps
+
+  private
+
+  def at_least_one_step?
+    errors.add(:base, 'must add at least one step') if steps.blank?
+  end
+
 end
