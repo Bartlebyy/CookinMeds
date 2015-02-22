5f686c2d3a8dcf1e82d36b3b341b85aa92c46c25
Roberta Reusch-Lewis <roberta@rigor.com>
2015-02-22 12:56:47 -0500

    fix spacing issue in recipe factory

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :recipe do
-  	name "A New Recipe"
-  	queued_for Time.now
-  	serving_size 2
+    name "A New Recipe"
+    queued_for Time.now
+    serving_size 2
 
     ignore do
       steps_count 5
